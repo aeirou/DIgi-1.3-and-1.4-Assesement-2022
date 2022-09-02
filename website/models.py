f1bb967293f53c1d84a4988f323737c7c5c04481
aeirone felipe <aeironefelipe@aeirones-MacBook-Air.local>
2022-09-02 15:04:57 +1200

    needs the foreign keys

--- website/models.py
+++ website/models.py
@@ -18,32 +18,79 @@ class Book(db.Model): #the whole table
     title = db.Column( #title of the books
       db.string(255),
       nullable=False,
-      unique=False,
+      unique=False
     )
     author = db.Column( #the author of the book
       db.string(225),
       nullable=False,
-      unique=False,
+      unique=False
     ) 
     year_published = db.Column( #the year the books were published
       db.datetime,
       nullable=False,
-      unique=False,
+      unique=False
     )
     isbn = db.Comlumn( #unqiue id at the back of the book
       db.integer,
       nullable = False,
-      unique=True,
+      unique=True
+    )
+    id_borrower = db.Column( #foreign key
+      db.Integer,
+      db.ForeignKey('borrower.id')
     )
 
-class borrowed(db.Model):
+class Borrower(db.Model):
 
-    __name__ = 'borrowed'
+    __name__ = 'borrower'
 
     id = db.Column(
       db.integer,
+      primary_key=True
+    )     
+    fname = db.Column( #first name of the borrower 
+      db.string(255),
+      nullable=False,
+      unique=False
+
+    )
+    lname = db.Column( #last name of the borrower
+      db.string(225),
+      nullable=False,
+      unique=False
+
+    )
+    email = db.Column( #email of the borrower
+      db.string(225),
       nullable=False
-    )    
+    )
+    ph_number = db.Column( #phone number of the user
+      db.integer,
+      nullable=False
+    )
+    borrower_id = db.Relationship(
+      "Book",
+      backref='borrower', #connects to the table
+      lazy=True
+    )
+
+class Borrowed_Books(db.Model):
+
+    __name__ = 'borrowed_books'
+    id = db.Column( #id of the borrowed book
+      db.Integer,
+      primary_key=True
+    )
+    loan_date = db.Columm(
+      db.datetime,
+      nullable=False
+    )
+    return_date = db.Column(
+      db.datetime,
+      nullable=False
+    ) 
+
+
 
 
 
